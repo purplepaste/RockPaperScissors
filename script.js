5e09f4cfb66313beb5de3adf67db068cf18139f1
purplepaste <bvstudyinbox@gmail.com>
2023-02-02 23:21:44 -0800

    Edit comments for clarity and re-organize unwritten modules and functions

--- script.js
+++ script.js
@@ -11,25 +11,47 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
+    // this function takes in user input in the form of one of three selectable values: rock, paper, or scissors //
+    // this function returns the selected value
+
     let input = prompt("Choose your weapon! Select rock, paper, or scissors.");
     let playerSelection = input.toLowerCase();
     return playerSelection;
     
-    // this function takes in user input in the form of one of three selectable values: rock, paper, or scissors //
-    // this function returns the selected value
+    
 }
 const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice();
 
-function checker(playerSelection) {
-    if (playerSelection != "rock" || "paper" || "scissors") {
-        console.log("Please chose an available weapon")
-        getPlayerChoice();
-    }
-    else return;
-}
+//function checker(playerSelection) {
+// this function takes in playerselection, 
+//compares it to one of the three available weapons, 
+//and asks for input if it is not equal to "rock", "paper", or "scissors".
+
+    //if (playerSelection != "rock" || "paper" || "scissors") {
+      //  console.log("Please chose an available weapon")
+        //getPlayerChoice();
+    //}
+  //  else return;
+//
 
 function game (computerSelection, playerSelection) {
+
+    //function compareSelections combined into "game" function
+   
+    //get return value of computerSelection
+    //get return value of playerSelection 
+    //compare computerWeapon to playerWeapon:
+        //if computerWeapon = rock && playerWeapon = rock, tie
+        //if computerWeapon = rock && playerWeapon = paper, player wins
+        //if computerWeapon = rock && playerWeapon = scissors, computer wins 
+        //if computerWeapon = paper && playerWeapon = rock, computer wins  
+        //if computerWeapon = paper && playerWeapon = paper, tie
+        //if computerWeapon = paper && playerWeapon = scissors, player wins  
+        //if computerWeapon = scissors && playerWeapon = rock, player wins
+        //if computerWeapon = scissors && playerWeapon = paper, computer wins
+        //if computerWeapon = scissors && playerWeapon = scissors, tie
+
     console.log("The computer chose " + computerSelection);
     //checker();
     console.log("You chose " + playerSelection);
@@ -65,26 +87,6 @@ function game (computerSelection, playerSelection) {
     }
 
 game(computerSelection, playerSelection);
-
-
-
-
-
-
-//function compareSelections()
-   
-    //get return value of getComputerChoice and store it in a variable computerWeapon //
-    //get return value of playerSelection and store it in a variable playerWeapon //
-    //compare computerWeapon to playerWeapon:
-        //if computerWeapon = rock && playerWeapon = rock, tie
-        //if computerWeapon = rock && playerWeapon = paper, player wins
-        //if computerWeapon = rock && playerWeapon = scissors, computer wins 
-        //if computerWeapon = paper && playerWeapon = rock, computer wins  
-        //if computerWeapon = paper && playerWeapon = paper, tie
-        //if computerWeapon = paper && playerWeapon = scissors, player wins  
-        //if computerWeapon = scissors && playerWeapon = rock, player wins
-        //if computerWeapon = scissors && playerWeapon = paper, computer wins
-        //if computerWeapon = scissors && playerWeapon = scissors, tie
 
 
 //function scoreboard() {
