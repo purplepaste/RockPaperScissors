64635951cb82d6756c2e99c3d200180805b4f918
purplepaste <bvstudyinbox@gmail.com>
2023-02-06 23:07:42 -0800

    Add scoreboard to determine winner and loser after 5 rounds.

--- script.js
+++ script.js
@@ -28,65 +28,88 @@ function getPlayerChoice() {
     }
 
 function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+
+    function scoreBoard (computerScore, playerScore) {
+        if (computerScore > playerScore) {
+            console.log("The computer scored " + computerScore + " points this match.");
+            console.log("You scored " + playerScore + " points this match.");
+            console.log("The machines have won!");
+        }
+
+        else if (computerScore < playerScore) {
+            console.log("The computer scored " + computerScore + " points this match.");
+            console.log("You scored " + playerScore + " points this match.");
+            console.log("You have secured victory for the humans!");
+        }
+
+        else console.log("The match is a draw. May man and machine find peace and harmony on earth.");
+    }
 
     for (let i = 0; i < 5; i ++) {
 
-    const computerSelection = getComputerChoice();
-    const playerSelection = getPlayerChoice();
-    
-         function playRound (computerSelection, playerSelection) {
-            console.log("The computer chose " + computerSelection);
-            //checker();
-            console.log("You chose " + playerSelection);
-            
-            let computerScore = 0;
-            let playerScore = 0;
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
 
-                if (computerSelection == "rock" && playerSelection == "paper"){
-                    console.log("You win!");
-                    playerScore ++;
-                    
-                }
-
-                else if (computerSelection == "rock" && playerSelection == "scissors") {
-                    console.log("The Computer wins!");
-                    computerScore ++;
-                    
-                }
-
-                else if (computerSelection == "paper" && playerSelection == "rock") {
-                    console.log("The Computer wins!");
-                    computerScore ++;
-                }
-
-                else if (computerSelection == "paper" && playerSelection == "scissors") {
-                    console.log("You win!");
-                    playerScore ++;
-                }
-
-                else if (computerSelection == "scissors" && playerSelection == "rock") {
-                    console.log("You win!");
-                    playerScore ++;
-                }
-
-                else if (computerSelection == "scissors" && playerSelection == "paper") {
-                    console.log("The Computer wins!");
-                    computerScore ++;
-                }
-                
-                else if (computerSelection == playerSelection) {
-                    console.log("It's a tie!");
-                }
-
-                console.log("You scored " + playerScore + " points.")
-                console.log("The computer scored " + computerScore + " points.")
-
-            
-
+        function checker (playerSelection) {
+            if (playerSelection !== "rock" || playerSelection !== "paper" || playerSelection !== "scissors") {
+                console.log("Please choose one of the available weapons.")
+                computerSelection = null;
+            }
+            getPlayerChoice;
         }
+        
+        
+        
+        function playRound (computerSelection, playerSelection) {
+            console.log("The computer chose " + computerSelection);
+            console.log("You chose " + playerSelection);  
+            
+            if (computerSelection == "rock" && playerSelection == "paper"){
+            console.log("You win!");
+            playerScore ++;
+            }
+            else if (computerSelection == "rock" && playerSelection == "scissors") {
+            console.log("The Computer wins!");
+            computerScore ++;
+                    
+            }
 
-        playRound (computerSelection, playerSelection);
-    }
+            else if (computerSelection == "paper" && playerSelection == "rock") {
+            console.log("The Computer wins!");
+            computerScore ++;
+            }
+
+            else if (computerSelection == "paper" && playerSelection == "scissors") {
+            console.log("You win!");
+            playerScore ++;
+            }
+
+            else if (computerSelection == "scissors" && playerSelection == "rock") {
+            console.log("You win!");
+            playerScore ++;
+            }
+            
+            else if (computerSelection == "scissors" && playerSelection == "paper") {
+            console.log("The Computer wins!");
+            computerScore ++;
+            }
+                    
+            else if (computerSelection == playerSelection) {
+            console.log("It's a tie!");
+            }
+        
+                    
+        }
+    
+    checker(playerSelection);
+    playRound (computerSelection, playerSelection);
+    }  
+
+    scoreBoard(computerScore, playerScore);
+
 }
+
 
 game();
